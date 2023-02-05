cc679cd1b66db65f50c65a39321450a61c3b13aa
BloggingBitcoin <103765434+BrutusBondBTC@users.noreply.github.com>
2023-02-04 17:31:14 -0800

    Create key_gen.js
    added file that creates a nostr key out a bitcoin seed phrase.

---
+++ key_gen.js
@@ -0,0 +1,26 @@
+[...new Uint8Array(await crypto.subtle.digest('SHA-256', new TextEncoder().encode('disorder dish either velvet insect session rigid rough work path bean release')))].map((b) => b.toString(16).padStart(2, '0')).join('')
+
+/*
+How To Genorate Nostr Keys Using A Bitcoin Key
+
+  1)  To get in the console, press Ctrl + Shift + J
+
+  2) Copy and paste the above code
+    
+  3) Replace the 12 seed with your own seed words
+    
+    You can also use the words for a bitcoin wallet, but remeber:  IT IS A HOT WALLET
+    
+   4)Press Enter
+
+
+If you have any questions hit me up on Nostr:
+
+npub1wkljx5c6a8uccc5etws8ry0y3r4dgavh2dcav0tal4rtmcdl4z2sfu5u0t
+
+Special thanks to SPA, @super_phat_arrow on twitter. If you find this software useful, please consider sending value to him.
+
+His nostr pubkey is:
+npub1fguyv0p2whngcfzpdemjpganhmamp6ngwt26q35jcw0p368jmjkqy27896
+
+*/
